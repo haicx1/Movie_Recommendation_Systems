4a3ec4371c69c15fa1ffb511e7b4924049583830
haicx1 <haicx981@gmail.com>
2024-03-07 17:01:48 +0700

    hello

--- review/models.py
+++ review/models.py
@@ -6,12 +6,18 @@ class Movie(models.Model):
     title = models.TextField()
     genres = models.TextField()
 
-    def __str__(self):
-        return f"{self.title} ({self.genres})"
+    class Meta:
+        db_table = 'movie'
+
+
+class Rating(models.Model):
+    user = models.ForeignKey('UserMovie', models.CASCADE, blank=True, null=True)
+    movie = models.ForeignKey(Movie, models.CASCADE, blank=True, null=True)
+    rating = models.IntegerField(blank=True, null=True)
+    timestamp = models.IntegerField(blank=True, null=True)
 
     class Meta:
-        managed = True
-        db_table = 'movie'
+        db_table = 'rating'
 
 
 class UserMovie(models.Model):
@@ -24,17 +30,4 @@ class UserMovie(models.Model):
     occ_desc = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = True
         db_table = 'user_movie'
-
-
-class Rating(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(UserMovie, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    rating = models.IntegerField(blank=True, null=True)
-    timestamp = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = True
-        db_table = 'rating'
